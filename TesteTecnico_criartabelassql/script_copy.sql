17311a0a7b6fc34c52a8f0706a13e7bb6ea45ce3
Carlos <carloshenrique20051bb@gmail.com>
2025-03-26 16:51:19 -0300

    Arrumando path

--- TesteTecnico_criartabelassql/script_copy.sql
+++ TesteTecnico_criartabelassql/script_copy.sql
@@ -1,8 +1,8 @@
-\copy testetecnico.primeirotrimestre2023 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\1T2023.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.segundotrimestre2023 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\2T2023.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.terceirotrimestre2023 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\3T2023.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.quartotrimestre2023 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\4T2023.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.primeirotrimestre2024 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\1T2024.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.segundotrimestre2024 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\2T2024.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.terceirotrimestre2024 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\3T2024.csv' WITH DELIMITER ';' CSV
-\copy testetecnico.quartotrimestre2024 from 'C:\Users\Carlos\Documents\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\4T2024.csv' WITH DELIMITER ';' CSV 
+\copy testetecnico.primeirotrimestre2023 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\1T2023.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.segundotrimestre2023 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\2T2023.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.terceirotrimestre2023 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\3T2023.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.quartotrimestre2023 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\4T2023.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.primeirotrimestre2024 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\1T2024.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.segundotrimestre2024 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\2T2024.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.terceirotrimestre2024 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\3T2024.csv' WITH DELIMITER ';' CSV
+\copy testetecnico.quartotrimestre2024 from '\TesteTecnico\TesteTecnico_criartabelassql\arquivos_csv\4T2024.csv' WITH DELIMITER ';' CSV 
